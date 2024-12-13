7a644862baf15bcd690cbe0293c8b0da71987dcc
Camdon Cady <camdon.cady@gmail.com>
2024-12-12 22:09:16 -0600

    Finish day 12. Long but it works.

--- day12/main.go
+++ day12/main.go
@@ -43,8 +43,151 @@ func (r region) perimeter() int {
 	return res
 }
 
+func (r region) sides() int {
+	topfences := make(map[point]bool)
+	bottomfences := make(map[point]bool)
+	leftfences := make(map[point]bool)
+	rightfences := make(map[point]bool)
+
+	for p := range r.points {
+		a := p.adjacent()
+		up, down, left, right := a[0], a[1], a[2], a[3]
+		if !r.points[up] {
+			topfences[p] = true
+		}
+		if !r.points[down] {
+			bottomfences[p] = true
+		}
+		if !r.points[left] {
+			leftfences[p] = true
+		}
+		if !r.points[right] {
+			rightfences[p] = true
+		}
+	}
+
+	res := 0
+	for len(topfences) > 0 {
+		// This is an absurd way to do this
+		var current point
+		for k := range topfences {
+			current = k
+			break
+		}
+		// Remove it from our fences
+		delete(topfences, current)
+
+		// Move left
+		left := point{current.x, current.y - 1}
+		for topfences[left] {
+			delete(topfences, left)
+			left = point{left.x, left.y - 1}
+		}
+
+		// Move right
+		right := point{current.x, current.y + 1}
+		for topfences[right] {
+			delete(topfences, right)
+			right = point{right.x, right.y + 1}
+		}
+
+		// Catalog it
+		res++
+	}
+
+	for len(bottomfences) > 0 {
+		// This is an absurd way to do this
+		var current point
+		for k := range bottomfences {
+			current = k
+			break
+		}
+		// Remove it from our fences
+		delete(bottomfences, current)
+
+		// Move left
+		left := point{current.x, current.y - 1}
+		for bottomfences[left] {
+			delete(bottomfences, left)
+			left = point{left.x, left.y - 1}
+		}
+
+		// Move right
+		right := point{current.x, current.y + 1}
+		for bottomfences[right] {
+			delete(bottomfences, right)
+			right = point{right.x, right.y + 1}
+		}
+
+		// Catalog it
+		res++
+	}
+
+	for len(leftfences) > 0 {
+		// This is an absurd way to do this
+		var current point
+		for k := range leftfences {
+			current = k
+			break
+		}
+		// Remove it from our hfences
+		delete(leftfences, current)
+
+		// Move up
+		up := point{current.x - 1, current.y}
+		for leftfences[up] {
+			delete(leftfences, up)
+			up = point{up.x - 1, up.y}
+		}
+
+		// Move down
+		down := point{current.x + 1, current.y}
+		for leftfences[down] {
+			delete(leftfences, down)
+			down = point{down.x + 1, down.y}
+		}
+
+		// Catalog it
+		res++
+	}
+
+	for len(rightfences) > 0 {
+		// This is an absurd way to do this
+		var current point
+		for k := range rightfences {
+			current = k
+			break
+		}
+		// Remove it from our hfences
+		delete(rightfences, current)
+
+		// Move up
+		up := point{current.x - 1, current.y}
+		for rightfences[up] {
+			delete(rightfences, up)
+			up = point{up.x - 1, up.y}
+		}
+
+		// Move down
+		down := point{current.x + 1, current.y}
+		for rightfences[down] {
+			delete(rightfences, down)
+			down = point{down.x + 1, down.y}
+		}
+
+		// Catalog it
+		res++
+	}
+
+	return res
+}
+
 func (r region) price() int {
 	return r.perimeter() * r.area()
+}
+
+func (r region) price2() int {
+	return r.sides() * r.area()
 }
 
 type gardenMap struct {
@@ -144,4 +287,11 @@ func main() {
 		ans1 += r.price()
 	}
 	log.Printf("The price for part one is %d", ans1)
+
+	ans2 := 0
+	for _, r := range regions {
+		ans2 += r.price2()
+	}
+	log.Printf("The price for part two is %d", ans2)
+
 }
