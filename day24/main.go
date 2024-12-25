553eb43d36fb87f14657edcf9fa45b076e6b590a
Camdon Cady <camdon.cady@gmail.com>
2024-12-25 17:51:59 -0600

    Day 24a. The elves are not proud of this one. Very much a WIP; requires manual intervention.

--- day24/main.go
+++ day24/main.go
@@ -30,6 +30,28 @@ type gate struct {
 	input2 string
 }
 
+func (g gate) set() gate {
+	return gate{
+		id:     g.id,
+		op:     g.op,
+		status: STATUS_TRUE,
+
+		input1: g.input1,
+		input2: g.input2,
+	}
+}
+
+func (g gate) unset() gate {
+	return gate{
+		id:     g.id,
+		op:     g.op,
+		status: STATUS_FALSE,
+
+		input1: g.input1,
+		input2: g.input2,
+	}
+}
+
 func newConst(id, status string) gate {
 	return gate{
 		id:     id,
@@ -148,6 +170,111 @@ func (c circuit) read(prefix string) int64 {
 	return res
 }
 
+func (c circuit) findOp(op, x, y string) (string, bool) {
+	for id, g := range c {
+		if g.op != op {
+			continue
+		}
+
+		if g.input1 == x && g.input2 == y {
+			return id, true
+		}
+
+		if g.input1 == y && g.input2 == x {
+			return id, true
+		}
+	}
+
+	return "", false
+}
+
+type adder struct {
+	carryIn  string
+	carryOut string
+
+	inputXor string
+	inputAnd string
+	carryAnd string
+}
+
+func findHalfAdder(c circuit) adder {
+	// The only half adder is the zero-th position
+	a := adder{"", "", "", "", ""}
+
+	if xorGate, ok := c.findOp(OP_XOR, "x00", "y00"); ok {
+		a.inputXor = xorGate
+	} else {
+		log.Fatal("Need to re-map gates to find the half adder; didn't find XOR")
+	}
+
+	if carryOut, ok := c.findOp(OP_AND, "x00", "y00"); ok {
+		a.carryOut = carryOut
+	} else {
+		log.Fatal("Need to re-map gates to find the half adder, didn't find AND")
+	}
+
+	return a
+}
+
+func findFullAdder(c circuit, idx int, carryHint string) adder {
+	xInput := fmt.Sprintf("x%02d", idx)
+	yInput := fmt.Sprintf("y%02d", idx)
+	zOutput := fmt.Sprintf("z%02d", idx)
+
+	inputXor, ok := c.findOp(OP_XOR, xInput, yInput)
+	if !ok {
+		log.Fatal("Need to do some swapping of the inputs to find XOR", xInput, yInput)
+	}
+
+	outputCandidate := c[zOutput]
+	if outputCandidate.op != OP_XOR {
+		outputCandidate2, ok := c.findOp(OP_XOR, carryHint, inputXor)
+		if !ok {
+			log.Fatal(zOutput, "gate is not a XOR and no substitute found")
+		}
+		log.Fatal("Need to swap ", zOutput, " and ", outputCandidate2)
+	}
+
+	if outputCandidate.input1 != carryHint && outputCandidate.input2 != carryHint {
+		log.Fatal("Carry-in", carryHint, "not part of", zOutput)
+	}
+
+	if outputCandidate.input1 != inputXor && outputCandidate.input2 != inputXor {
+		needToSwap := ""
+		if outputCandidate.input1 == carryHint {
+			needToSwap = outputCandidate.input2
+		} else {
+			needToSwap = outputCandidate.input1
+		}
+		log.Println("Output of ", xInput, " and ", yInput, " doesn't go to ", zOutput)
+		log.Fatal("Try swapping ", needToSwap, " and ", inputXor)
+	}
+
+	inputAnd, ok := c.findOp(OP_AND, xInput, yInput)
+	if !ok {
+		log.Fatal("Need to do some swapping of input to find AND", xInput, yInput)
+	}
+
+	carryAnd, ok := c.findOp(OP_AND, inputXor, carryHint)
+	if !ok {
+		log.Fatal("Didn't find a carryAnd between", inputXor, "and", carryHint)
+	}
+
+	carryOut, ok := c.findOp(OP_OR, carryAnd, inputAnd)
+	if !ok {
+		log.Fatal("Didn't find a carrout OR gate between", carryAnd, "and", inputAnd)
+	}
+
+	return adder{
+		carryIn:  carryHint,
+		carryOut: carryOut,
+
+		inputXor: inputXor,
+		inputAnd: inputAnd,
+		carryAnd: carryAnd,
+	}
+}
+
 func main() {
 	fname := "example.txt"
 
@@ -192,5 +319,33 @@ func main() {
 	ans1 := solution1.read("z")
 
 	log.Println("The answer to part one is", ans1)
+
+	// Upon inspection, this is a n-bit ripple-carry adder. That has one half adder (z00 <- x00 XOR y00; carry00 <- x00 AND y00), and n-1 full adders.
+	// Each full adder is composed of:
+	// - A partial computation of zNN by computing xNN XOR yNN, possibly with input order reversed
+	// - An XOR of that partial computation with the carry of the adder for the next-lowest bit
+	//
+	// There are a few ways that swaps can need to occur:
+	// - If the wrong input bits are XOR'd together, we need to swap an input. To do that we have to figure out which swap to take
+	// - If the z bits appear out of order, we need to swap those
+	// - Probably the same thing can happen with the partials and the carries
+
+	circ2 := circuit{}
+	for k, v := range circ {
+		circ2[k] = v
+	}
+
+	halfAdder := findHalfAdder(circ2)
+	delete(circ2, halfAdder.inputXor)
+	delete(circ2, "x00")
+	delete(circ2, "y00")
+
+	lastCarry := halfAdder.carryOut
+
+	for i := 1; i < 45; i++ {
+		fullerAdder := findFullAdder(circ2, i, lastCarry)
+		lastCarry = fullerAdder.carryOut
+		log.Println("Successful on iteration", i)
+	}
 
 }
